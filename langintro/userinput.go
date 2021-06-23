6875a3ae2d1c7d8537ea1f132b008b8d6926183d
abp437 <abp437@gmail.com>
2021-06-23 15:59:32 +0530

    Add util function for getting CLI input

--- langintro/userinput.go
+++ langintro/userinput.go
@@ -7,12 +7,17 @@ import (
 	"strings"
 )
 
+func cliInput(r *bufio.Reader, p string) (string, error) {
+	fmt.Print(p)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
 // CreateBill creates a bill through user input and then sends it back to the user
 func CreateBill() Bill {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter your name")
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
+	name, _ := cliInput(reader, "Enter your name: ")
 
 	return newBill(name)
 }
