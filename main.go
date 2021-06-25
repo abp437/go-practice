4c8ba0a926bbdad0cca17a388c5ac1a518c2247a
abp437 <abp437@gmail.com>
2021-06-25 12:45:44 +0530

    Add Notes file for each package, add cards package

--- main.go
+++ main.go
@@ -1,39 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"go-practice/langintro"
-)
-
-func sayGreeting(name string) {
-	fmt.Println("Good morning", name)
-}
+import "fmt"
 
 func main() {
-	langintro.PrintHelloWorld()
-	langintro.PrintHelloNinjas()
-
-	marioCharactersList := langintro.MarioCharacters()
-	fmt.Println(marioCharactersList, len(marioCharactersList))
-
-	langintro.NormalLoop(5)
-	langintro.RangeLoop()
-
-	langintro.HigherOrderFunction("Wario", sayGreeting)
-	fmt.Println(langintro.CircleArea(34.6))
-	fn, ln := langintro.PersonInitials("Akshay Pawar")
-	fmt.Printf("%v. %v. \n", fn, ln)
-	fmt.Println(langintro.MyName)
-
-	fmt.Println(langintro.ModifyName("Akshay"))
-
-	shapes := []langintro.Shape{
-		langintro.Circle{Radius: 20.3},
-		langintro.Square{Length: 20.3},
-	}
-	for _, s := range shapes {
-		langintro.ShapeInfo(s)
-	}
-
-	langintro.CreateBill()
+	fmt.Println("Hello World")
 }
