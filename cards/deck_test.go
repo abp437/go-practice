7caded8029260cb69b28f5a40a7539876ec1e2b2
abp437 <abp437@gmail.com>
2021-06-27 13:40:07 +0530

    Add Basic Tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,1 +1,15 @@
 package cards
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := NewDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expect Deck length of 52, but got %v", len(d))
+	}
+}
+
+// Notes:
+// You can write tests around every function
+// We need to export all the test functions to be accessible on terminal
