d8d12a93cf0a81e6415f1eafb11a345bbdae108e
abp437 <abp437@gmail.com>
2021-06-27 13:54:46 +0530

    Add more tests to Deck

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -5,8 +5,19 @@ import "testing"
 func TestNewDeck(t *testing.T) {
 	d := NewDeck()
 
+	// Deck Size
 	if len(d) != 52 {
-		t.Errorf("Expect Deck length of 52, but got %v", len(d))
+		t.Errorf("Expected Deck length of 52, but got %v", len(d))
+	}
+
+	// First Card in Deck
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spade sto be the first card in the deck, instead got %v", d[0])
+	}
+
+	// Last Card in Deck
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected King of Clubs to be the last card in the deck, instead got %v", d[len(d)-1])
 	}
 }
 
